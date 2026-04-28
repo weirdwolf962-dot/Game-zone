98b484d5d9ae9bc5a9a7cf33d18d4e6b67df0499
weirdwolf962-dot <weirdwolf962@gmail.com>
2026-04-28 06:42:53 +0530

    Fix import path for VoiceBar in SpyGame.jsx

--- gamezone/src/components/games/spy/SpyGame.jsx
+++ gamezone/src/components/games/spy/SpyGame.jsx
@@ -6,6 +6,8 @@ import { ref, onValue, update, remove, get } from 'firebase/database'
 import { useAuth } from '../../../lib/AuthContext'
 import { generateWords } from '../../../lib/wordgen'
 import './SpyGame.css'
+import VoiceBar from '../shared/VoiceBar'   // in Room.jsx
+import VoiceBar from '../../shared/VoiceBar' // in SpyGame.jsx
 
 export default function SpyGame() {
   const { code } = useParams()
